209040163c7a01790c836e08a7ac9ae1fb907b30
root <root@DESKTOP-H3T39UL.localdomain>
2022-02-16 16:49:55 -0300

    Validating user data

--- src/models/User.js
+++ src/models/User.js
@@ -3,10 +3,39 @@ import Sequelize, { Model } from 'sequelize';
 export default class User extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password_hash: Sequelize.STRING,
-      password: Sequelize.VIRTUAL,
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Name field must be between 3 and 255 characters',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'Invalid email',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'Password field must be between 6 and 50 characters',
+          },
+        },
+      },
     }, {
       sequelize,
     });
