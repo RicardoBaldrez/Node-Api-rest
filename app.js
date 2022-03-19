69093fc3fa61c6d08993c764dc2dc5621be10277
root <root@DESKTOP-H3T39UL.localdomain>
2022-03-19 15:56:29 -0300

    Generating the user's JWT(token)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import express from 'express';
 
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
+import tokenRoutes from './src/routes/tokenRoutes';
 
 dotenv.config();
 
@@ -23,7 +24,8 @@ class App {
 
   routes() {
     this.app.use('/', homeRoutes);
-    this.app.use('/users', userRoutes);
+    this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
   }
 }
 
--- src/config/database.js
+++ src/config/database.js
@@ -7,15 +7,15 @@ module.exports = {
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
-define: {
-  timestamps: true,
-  underscored: true,
-  underscoredAll: true,
-  'createdAt': 'created_at',
-  'updatedAt': 'updated_at',
-},
-dialectOptions: {
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    'createdAt': 'created_at',
+    'updatedAt': 'updated_at',
+  },
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo'
+  },
   timezone: 'America/Sao_Paulo'
-},
-timezone: 'America/Sao_Paulo'
 }
